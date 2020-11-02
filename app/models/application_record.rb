a6d8326a250090f0bea6a7c0590dfc9a8c8abace
sutogone <batorukunn@yahoo.co.jp>
2020-11-02 19:11:49 +0900

    mail,password以外のカラムにストロングパラメータを設定
    前のcommitでapplication_controllerではないところに記述をしていたので、修正。

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,10 +1,3 @@
 class ApplicationRecord < ActiveRecord::Base
   self.abstract_class = true
-  before_action :configure_permitted_parameters, if: :devise_controller?
-
-  private
-  def configure_permitted_parameters
-  devise_parameter_sanitizer.permit(:sign_up, keys: [nickname, last_name, first_name, last_name_kana, first_name_kana, birthday,])
-  #ログイン時のmailとpassword以外のカラムのストロングパラメータを設定
-  end
 end
