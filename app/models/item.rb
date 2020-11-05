e812903cf01f3e3b79c4506c5f5f52dfd6efe5fa
sutogone <batorukunn@yahoo.co.jp>
2020-11-06 06:25:51 +0900

    active_hashの中身を記述
    active_hashのモデル、アソシエーション、バリデーションを設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,13 +1,22 @@
 class Item < ApplicationRecord
+  belongs_to :user
+  has_one :purchase
 
   with_options presence: true do
     validates :name, format: { with: {1,40} }
     validates :info, format: { with: {1,1000} }
+    validates :item_price, format: { with:  }
   end
 
+  with_options numericality_of: {other_then: 1} do
+    validates :category_id
+    validates :sales_status_id
+    validates :shipping_fee_id
+    validates :scheduled_delivery_id
+    validates :prefecture_id
+  end
 
-  belongs_to :user
-  has_one :purchase
+  extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to_active_hash :category
   belongs_to_active_hash :sales_status
   belongs_to_active_hash :shipping_fee
