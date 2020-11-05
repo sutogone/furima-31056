7331bfe9eeb4dd51dc228d281c039ffd72c50af0
sutogone <batorukunn@yahoo.co.jp>
2020-11-05 11:41:22 +0900

    バリデーションの設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,16 @@
 class Item < ApplicationRecord
+
+  with_options presence: true do
+    validates :name, format: { with: {1,40} }
+    validates :info, format: { with: {1,1000} }
+  end
+
+
+  belongs_to :user
+  has_one :purchase
+  belongs_to_active_hash :category
+  belongs_to_active_hash :sales_status
+  belongs_to_active_hash :shipping_fee
+  belongs_to_active_hash :scheduled_delivery
+  belongs_to_active_hash :prefecture
 end
