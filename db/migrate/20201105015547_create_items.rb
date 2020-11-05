554f6d65ec9436a609d918d27f870cb5ef782828
sutogone <batorukunn@yahoo.co.jp>
2020-11-05 11:14:27 +0900

    migrationファイルを記述

--- db/migrate/20201105015547_create_items.rb
+++ db/migrate/20201105015547_create_items.rb
@@ -1,7 +1,15 @@
 class CreateItems < ActiveRecord::Migration[6.0]
   def change
     create_table :items do |t|
-
+      t.string :name,                     null: false
+      t.text :info,                       null: false
+      t.integer :category_id,             null: false
+      t.integer :sales_status_id,         null: false
+      t.integer :shipping_fee_id,         null: false
+      t.integer :scheduled_delivery_id,   null: false
+      t.integer :item_price,              null: false
+      t.integer :prefecture_id,           null: false
+      t.references :user,                 foreign_key: true, null: false
       t.timestamps
     end
   end
