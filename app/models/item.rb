dc81f722d558d542e3ed134ccc4a1d66ff08c0ec
sutogone <batorukunn@yahoo.co.jp>
2020-11-06 06:54:14 +0900

    item_priceのバリデーションを見直し
    その他バリデーションの記述を見直し

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,25 +1,21 @@
 class Item < ApplicationRecord
-  belongs_to :user
-  has_one :purchase
-
-  with_options presence: true do
-    validates :name, format: { with: {1,40} }
-    validates :info, format: { with: {1,1000} }
-    validates :item_price, format: { with:  }
-  end
-
-  with_options numericality_of: {other_then: 1} do
-    validates :category_id
-    validates :sales_status_id
-    validates :shipping_fee_id
-    validates :scheduled_delivery_id
-    validates :prefecture_id
-  end
-
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to_active_hash :category
   belongs_to_active_hash :sales_status
   belongs_to_active_hash :shipping_fee
   belongs_to_active_hash :scheduled_delivery
   belongs_to_active_hash :prefecture
+  belongs_to :user
+  has_one :purchase
+
+  with_options presence: true do
+    validates :name,                    length: { in: 1..40 }
+    validates :info,                    length: { in: 1..1000 }
+    validates :item_price,              numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999 }
+    validates :category_id,             numericality: { other_then: 1 }
+    validates :sales_status_id,         numericality: { other_then: 1 }
+    validates :shipping_fee_id,         numericality: { other_then: 1 }
+    validates :scheduled_delivery_id,   numericality: { other_then: 1 }
+    validates :prefecture_id,           numericality: { other_then: 1 }
+  end
 end
