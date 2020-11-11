067fe0052276ab0b120f7a13ed8c5ae03c239c5d
sutogone <batorukunn@yahoo.co.jp>
2020-11-11 12:01:02 +0900

    コントローラーアクションを定義

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -1,2 +1,16 @@
 class PurchasesController < ApplicationController
+  before_action :set_item, only: [:index, :create]
+  before_action :authenticate_user!
+
+  #購入ページ
+  def index
+    
+  end
+  #/購入ページ
+  
+  #購入処理
+  def create
+    
+  end
+  #/購入処理
 end
